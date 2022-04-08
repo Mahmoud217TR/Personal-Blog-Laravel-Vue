11000d2b86098e528620806f08fb79ece1efe3e1
Mahmoud Mahmoud <54012843+Mahmoud217TR@users.noreply.github.com>
2022-04-09 01:00:15 +0300

    pagination controls for home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,5 +13,27 @@
             @endforelse
         </div>
     </div>
+    <div class="row">
+        <div class="col d-flex justify-content-center">
+            <!-- Previous link -->
+            @if($posts->currentPage() > 1)
+                <a class="btn btn-primary" href="{{ $posts->previousPageUrl() }}">Previous</a>
+            @else
+                <a class="btn btn-secondary disabled-btn" href="#">Previous</a>
+            @endif
+
+            <!-- Current Page Number -->
+            <div class="d-flex align-items-center">
+                <span class="badge bg-primary rounded-pill mx-2"> {{ $posts->currentPage() }} </span>
+            </div>
+
+            <!-- Next link -->
+            @if($posts->hasMorePages())
+                <a class="btn btn-primary" href="{{ $posts->nextPageUrl() }}">Next</a>
+            @else
+                <a class="btn btn-secondary disabled-btn" href="#">Next</a>
+            @endif
+        </div>
+    </div>
 </div>
 @endsection
