323bbe10c8f00a260427bcf4cded8c6cfc73a809
Mahmoud Mahmoud <54012843+Mahmoud217TR@users.noreply.github.com>
2022-04-20 00:07:10 +0300

    favorite to sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -3,45 +3,15 @@
     @include('layouts.nav')
     <hr class="hr-x2">
     <div class="h2 text-md-start text-center mt-2">
-        Instructions:
+        Favorite Posts:
     </div>
     <ol class="unstyled-list">
-      <li>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-      </li>
-      <li>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-      </li>
-      <li>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-      </li>
-      <li>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-      </li>
+        @forelse (\App\Models\Post::whereHasFavorite(auth()->user())->get() as $favorite_post)
+            <li>
+                <p><a class="text-light" href="{{ route('post.show',$favorite_post) }}">{{ $favorite_post->title }}</a></p>
+            </li>    
+        @empty
+            <p>No favorites to show.</p>
+        @endforelse
     </ol>
-    <hr class="hr-x2">
-    <div class="h2 text-md-start text-center mt-2">
-        Rules:
-    </div>
-    <ol class="unstyled-list">
-      <li>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-      </li>
-      <li>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-      </li>
-      <li>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-      </li>
-      <li>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-    </li>
-    <li>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-    </li>
-    <li>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-    </li>
-    </ol>
-    <hr class="hr-x2">
 </aside>
